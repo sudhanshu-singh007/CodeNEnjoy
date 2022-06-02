6ead5e975e941ed91dc09df7d23499aebddb7c7e
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-02 14:44:53 +0530

    adding singleton design pattern example

---
+++ src/main/java/designPatterns/sigletonDesignPatternEg.java
@@ -0,0 +1,73 @@
+package designPatterns;
+//The singleton pattern is a design pattern that restricts the instantiation of a class to one object
+
+//constructor private
+//public method of returning instance
+//instance type - private static
+
+class SingletonEagar {
+	  private static SingletonEagar instance = new SingletonEagar(); 
+	  
+	  private SingletonEagar(){}
+	  
+	  public static SingletonEagar getInstance() {
+	    return instance;
+	  }
+	}
+
+	class Singleton {
+	  private static Singleton instance; 
+	  
+	  private Singleton(){}
+	  
+	  public static Singleton getInstance() {
+	    if(instance == null) {
+	      instance = new Singleton();
+	    }
+	    
+	    return instance;
+	  }
+	}
+
+	class SingletonSynchronizedMethod {
+	  private static SingletonSynchronizedMethod instance; 
+	  
+	  private SingletonSynchronizedMethod(){}
+	  
+	  public static synchronized SingletonSynchronizedMethod getInstance() {
+	    if(instance == null) {
+	      instance = new SingletonSynchronizedMethod();
+	    }
+	    return instance;
+	  }
+	}
+
+	class SingletonSynchronized {
+	  private static SingletonSynchronized instance; 
+	  
+	  private SingletonSynchronized(){}
+	  
+	  public static SingletonSynchronized getInstance() {
+	    if(instance == null) {
+	      synchronized (SingletonSynchronized.class) {
+	        if(instance == null) {
+	          instance = new SingletonSynchronized();
+	        }
+	      }
+	    }
+	    return instance;
+	  }
+	}
+
+	public class sigletonDesignPatternEg {
+
+	  public static void main(String[] args) {
+	    SingletonSynchronized instance = SingletonSynchronized.getInstance();
+	    
+	    System.out.println(instance);
+	    
+	    SingletonSynchronized instance1 = SingletonSynchronized.getInstance();
+	    
+	    System.out.println(instance1);
+	  }
+	}
