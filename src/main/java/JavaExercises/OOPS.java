5277d8a07877bd89da6e54f1da23c006daf110c2
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-04 11:25:17 +0530

    Added oops class file

---
+++ src/main/java/JavaExercises/OOPS.java
@@ -0,0 +1,48 @@
+package JavaExercises;
+
+abstract class OOPS
+{
+    abstract void paint();
+    void display(){
+        System.out.println("Concrete method");
+    }
+}
+
+class A extends OOPS{
+    @Override
+    void paint(){
+        System.out.println("Paint in class JavaExercises.A");
+    }
+    void paint(String s){
+        System.out.println("Overloaded paint in class JavaExercises.A");
+    }
+}
+
+class B extends A
+{
+    B(){
+        super();
+    }
+
+    private String paintColor;
+    void setPaintColor(String paintColor){
+        this.paintColor = paintColor;
+    }
+    String getPaintColor(){
+        return paintColor;
+    }
+    @Override
+    void paint(String paintColor){
+        System.out.println("Overridden paint() in class JavaExercises.B with setColor(): "+getPaintColor());
+    }
+}
+
+
+class TestMethods{
+    public static void main(String[] args) {
+        B b = new B();
+        b.setPaintColor("Blue");
+        b.paint(b.getPaintColor());
+        b.paint();
+    }
+}
