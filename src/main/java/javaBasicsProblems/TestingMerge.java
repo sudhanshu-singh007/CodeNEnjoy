24f95e4c0b356805f7f9514cce69c38eb859e811
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-07 22:38:07 +0530

    testing merge

---
+++ src/main/java/javaBasicsProblems/TestingMerge.java
@@ -0,0 +1,11 @@
+package javaBasicsProblems;
+
+public class TestingMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing merge");
+
+	}
+
+}
