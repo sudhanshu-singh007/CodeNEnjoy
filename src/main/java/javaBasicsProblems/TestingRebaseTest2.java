79d7203d8eda718c7a5d93696f62dd6970b6c9ec
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-08 10:47:05 +0530

    second commit in test to check rebase feature

---
+++ src/main/java/javaBasicsProblems/TestingRebaseTest2.java
@@ -0,0 +1,12 @@
+package javaBasicsProblems;
+
+public class TestingRebaseTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("second commit in test to check rebase feature");
+		
+
+	}
+
+}
