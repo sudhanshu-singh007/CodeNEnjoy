177eceae1cff5807a41e161cd5f8d9f06f2871d5
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-11 23:09:54 +0530

    removed code

--- src/main/java/javaBasicsProblems/TestCheery1.java
+++ src/main/java/javaBasicsProblems/TestCheery1.java
@@ -5,8 +5,6 @@ public class TestCheery1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("for test");
-		System.out.println("ftest passed");
-
 	}
 
 }
