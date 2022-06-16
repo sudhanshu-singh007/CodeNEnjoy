5da3b6125883960933ca51d375994b5027a6fc6c
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-16 22:10:58 +0530

    remove the comment

--- src/main/java/javaBasicsProblems/PrimeNumberProblem.java
+++ src/main/java/javaBasicsProblems/PrimeNumberProblem.java
@@ -4,7 +4,7 @@ package javaBasicsProblems;
 public class PrimeNumberProblem {
 	public static void main(String args[]) {
 		int i, m = 0, flag = 0;
-		int n = 3;// it is the number to be checked
+		int n = 3;
 		m = n / 2;
 		if (n == 0 || n == 1) {
 			System.out.println(n + " is not prime number");
