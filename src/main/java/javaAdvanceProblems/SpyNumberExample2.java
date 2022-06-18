1a7f7ae6057cc9be6275c07d2429317acc166e1b
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-18 12:52:43 +0530

    modified the code

--- src/main/java/javaAdvanceProblems/SpyNumberExample2.java
+++ src/main/java/javaAdvanceProblems/SpyNumberExample2.java
@@ -8,15 +8,10 @@ public class SpyNumberExample2 {
 		int lastDigit = 0;
 		int sum = 0;
 		int product = 1;
-//executes until the condition returns true  
 		while (number != 0) {
-//determines the last digit of the given number  
 			lastDigit = number % 10;
-//adds the last digit to the variable sum  
 			sum = sum + lastDigit;
-//multiply last digit with product  
 			product = product * lastDigit;
-//removes the last digit of the given number  
 			number = number / 10;
 		}
 //compares the variable sum with product and returns the result accordingly  
