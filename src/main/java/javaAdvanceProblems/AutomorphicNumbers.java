2bfb7c11fa59dd00797cb7a5966c73eb20033272
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-21 14:06:16 +0530

    removed comments

--- src/main/java/javaAdvanceProblems/AutomorphicNumbers.java
+++ src/main/java/javaAdvanceProblems/AutomorphicNumbers.java
@@ -3,22 +3,15 @@ package javaAdvanceProblems;
 import java.util.Scanner;
 
 public class AutomorphicNumbers {
-//user-defined static method that checks whether the number is automorphic or not  
 	private static boolean isAutomorphic(int num) {
 		int count = 0;
-//determines the square of the given number  
 		int square = num * num;
-//copying the variable num into temp  
 		int temp = num;
-//iterate over the variable num until the condition become false  
 		while (temp > 0) {
 			count++;
-//removes last digit of the variable num  
 			temp = temp / 10;
 		}
-//determines the last digit of the variable square  
 		int lastDigit = (int) (square % (Math.pow(10, count)));
-//compare the last digit with num  
 		return num == lastDigit;
 	}
 
@@ -30,11 +23,8 @@ public class AutomorphicNumbers {
 		System.out.print("Enter the ending value: ");
 		end = in.nextInt();
 		System.out.println("Automorphic numbers between " + start + " and " + end + " are: ");
-//the for loop starts from the starting value and execute until the condition becomes false  
 		for (int i = start; i <= end; i++) {
-//calling the user-defined method      
 			if (isAutomorphic(i))
-//prints the number if it is automorphic  
 				System.out.print(i + " ");
 		}
 	}
