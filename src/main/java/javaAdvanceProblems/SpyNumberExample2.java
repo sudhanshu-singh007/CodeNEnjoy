d944b9d9c6ca755dc5214615bfc722f81411aff3
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-23 00:29:55 +0530

    modified the code

--- src/main/java/javaAdvanceProblems/SpyNumberExample2.java
+++ src/main/java/javaAdvanceProblems/SpyNumberExample2.java
@@ -3,7 +3,6 @@ package javaAdvanceProblems;
 import java.util.Scanner;
 
 public class SpyNumberExample2 {
-//method to check the Spy number  
 	private static boolean isSpyNumber(int number) {
 		int lastDigit = 0;
 		int sum = 0;
@@ -14,27 +13,21 @@ public class SpyNumberExample2 {
 			product = product * lastDigit;
 			number = number / 10;
 		}
-//compares the variable sum with product and returns the result accordingly  
 		if (sum == product)
 			return true;
 		return false;
 	}
 
-//driver code  
 	public static void main(String args[]) {
 		int lowerRange = 0, upperRange = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the lower range: ");
-//reads lower range  
 		lowerRange = sc.nextInt();
 		System.out.print("Enter upper range: ");
-//reads the upper range   
 		upperRange = sc.nextInt();
 		System.out.println("The Spy numbers between " + lowerRange + " to " + upperRange + " are: ");
 		for (int i = lowerRange; i <= upperRange; i++) {
-//calling user-defined function that checks if the given number is spy or not  
 			if (isSpyNumber(i))
-//prints all the spy numbers  
 				System.out.print(i + " ");
 		}
 	}
