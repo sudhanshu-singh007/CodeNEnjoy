cd1498d596a7d2d343a76d811eb80b6ee57299d0
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-23 00:32:58 +0530

    removed unwanted comments and spaces

--- src/main/java/javaBasicsProblems/RandomNumberExample5.java
+++ src/main/java/javaBasicsProblems/RandomNumberExample5.java
@@ -5,16 +5,13 @@ public class RandomNumberExample5 {
 	public static void main(String[] args) {
 		randomInts(5);
 		randomInts(9, 50, 90);
-//getStreamOfRandomInts(30, 50);  
 	}
 
-//method that generates a stream of integers having size 5  
 	public static void randomInts(int num) {
 		Random random = new Random();
 		random.ints(num).forEach(System.out::println);
 	}
 
-//method that generates a stream of 9 integers between 50 to 90  
 	public static void randomInts(int num, int origin, int bound) {
 		Random random1 = new Random();
 		random1.ints(num, origin, bound).forEach(System.out::println);
