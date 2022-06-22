e7d2997810ad27e565989903067c9a8562bf0d24
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-23 00:31:20 +0530

    chenge the comments

--- src/main/java/javaBasicsProblems/ArmstsrongNumberExample.java
+++ src/main/java/javaBasicsProblems/ArmstsrongNumberExample.java
@@ -17,27 +17,20 @@ public class ArmstsrongNumberExample {
 			sum += (Math.pow(last, digits));
 			temp = temp / 10;
 		}
-//compares the sum with n  
 		if (n == sum)
-//returns if sum and n are equal  
 			return true;
-//returns false if sum and n are not equal  
 		else
 			return false;
 	}
 
-//driver code  
 	public static void main(String args[]) {
 		int num;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the limit: ");
-//reads the limit from the user  
 		num = sc.nextInt();
 		System.out.println("Armstrong Number up to " + num + " are: ");
 		for (int i = 0; i <= num; i++)
-//function calling  
 			if (isArmstrong(i))
-//prints the armstrong numbers  
 				System.out.print(i + ", ");
 	}
 }
