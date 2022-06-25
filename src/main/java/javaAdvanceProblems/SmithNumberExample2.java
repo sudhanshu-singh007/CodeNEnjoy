e34bb9af0eaf39bb90afeed09e3102b879d9134d
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-25 13:50:07 +0530

    added smith number

--- src/main/java/javaAdvanceProblems/SmithNumberExample2.java
+++ src/main/java/javaAdvanceProblems/SmithNumberExample2.java
@@ -3,9 +3,7 @@ package javaAdvanceProblems;
 import java.util.*;
 
 public class SmithNumberExample2 {
-//function finds all the prime factors for the given number  
 	static List<Integer> findPrimeFactors(int n) {
-//creating an array list that stores the prime factors      
 		List<Integer> result = new ArrayList<>();
 		for (int i = 2; n % i == 0; n = n / i)
 			result.add(i);
@@ -16,32 +14,22 @@ public class SmithNumberExample2 {
 			}
 		}
 		if (n != 1)
-//the add() method adds the prime factors to the list  
 			result.add(n);
 		return result;
 	}
 
-//function finds the sum of digits of the given number  
 	static int sumOfDigits(int n) {
 		int sum = 0;
 		while (n > 0) {
-//finds the last digit and add it to the sum      
 			sum = sum + (n % 10);
-//removes the last digit from the number  
 			n = n / 10;
 		}
-//returns the sum of digits  
 		return sum;
 	}
 
-//driver code      
 	public static void main(String args[]) {
-//finds all smit numbers up to 10000      
 		for (int n = 1; n < 10000; n++) {
-//calling the user-defined function that finds prime factors  
 			List<Integer> factors = findPrimeFactors(n);
-//the size() method returns the number of elements in the list  
-//executes until the condition becomes false  
 			if (factors.size() > 1) {
 				int sum = sumOfDigits(n);
 				for (int f : factors)
