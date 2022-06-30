a575d03c93b1ee6ed7f55826a013aae2b658400d
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-30 17:48:33 +0530

    removed comment

--- src/main/java/javaBasicsProblems/FactorialProgram.java
+++ src/main/java/javaBasicsProblems/FactorialProgram.java
@@ -3,7 +3,7 @@ package javaBasicsProblems;
 class FactorialExample {
 	public static void main(String args[]) {
 		int i, fact = 1;
-		int number = 5;// It is the number to calculate factorial
+		int number = 5;
 		for (i = 1; i <= number; i++) {
 			fact = fact * i;
 		}
