af6cca98743dd4da302635e8b5dc3dc7d3885892
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-30 17:47:49 +0530

    removed comment

--- src/main/java/javaAdvanceProblems/StrontioNumberExample2.java
+++ src/main/java/javaAdvanceProblems/StrontioNumberExample2.java
@@ -9,13 +9,9 @@ public class StrontioNumberExample2 {
 		int num = sc.nextInt();
 		if ((num > 999) && (num < 10000)) {
 			int n = num;
-//multiplies the given number by 2  
 			num = num * 2;
-//removes first digit  
 			num = num / 10;
-//removes last digit  
 			num = num % 100;
-//compares the quotient and remainder   
 			if ((num / 10) == (num % 10))
 				System.out.println(n + " is a strontio number.");
 			else
