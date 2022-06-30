0aebf4e889fe3c759a55953ef8a38e4958c8c26c
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-30 17:48:59 +0530

    removed comment

--- src/main/java/javaBasicsProblems/FibonacciProblem.java
+++ src/main/java/javaBasicsProblems/FibonacciProblem.java
@@ -3,9 +3,9 @@ package javaBasicsProblems;
 class FibonacciExample1 {
 	public static void main(String args[]) {
 		int n1 = 0, n2 = 1, n3, i, count = 10;
-		System.out.print(n1 + " " + n2);// printing 0 and 1
+		System.out.print(n1 + " " + n2);
 
-		for (i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
+		for (i = 2; i < count; ++i)
 		{
 			n3 = n1 + n2;
 			System.out.print(" " + n3);
@@ -15,7 +15,6 @@ class FibonacciExample1 {
 
 	}
 }
-//using recursion
 class FibonacciExample2{  
 	 static int n1=0,n2=1,n3=0;    
 	 static void printFibonacci(int count){    
@@ -29,7 +28,7 @@ class FibonacciExample2{
 	 }    
 	 public static void main(String args[]){    
 	  int count=10;    
-	  System.out.print(n1+" "+n2);//printing 0 and 1    
-	  printFibonacci(count-2);//n-2 because 2 numbers are already printed   
+	  System.out.print(n1+" "+n2);
+	  printFibonacci(count-2); 
 	 }  
 	}  
