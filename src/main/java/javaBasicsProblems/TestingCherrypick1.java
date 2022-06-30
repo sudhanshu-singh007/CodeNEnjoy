400636a216bad4a63e335c2b42d15582eb7a1e15
Sudhanshu <sudhanshu_singh@epam.com>
2022-06-30 22:29:11 +0530

    modified the code

--- src/main/java/javaBasicsProblems/TestingCherrypick1.java
+++ src/main/java/javaBasicsProblems/TestingCherrypick1.java
@@ -5,10 +5,5 @@ public class TestingCherrypick1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("for test");
-		System.out.println("for test");
-		
-		System.out.println("for test");
-		System.out.println("for test");
 	}
-
 }
